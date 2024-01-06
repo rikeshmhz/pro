49013d42e0403d6cba26e0ca856ac08149fd08cf
rikesh maharjan <rikeshmaharjan22@gmail.com>
2024-01-06 08:28:06 +0545

    text-justify

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,11 +7,11 @@ const About = () => {
                 <div className='mb-10'>
                     <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
                 </div>
-                <p>
+                <p className='text-justify'>
                     My educational journey started at <b>Whitefield Higher Secondary School</b>, where I laid the foundation of my curiosity and thirst for knowledge. I joined <b>Prime College</b> for my +2 education, where I began exploring the fascinating world of programming. Fast forward I continued my bachelor's degree at <b>Prime College</b> where I further honed my skills and expanded my horizons. Armed with a degree and an insatiable appetite for coding, I embarked on a journey that led me to become proficient in an array of programming languages.
                 </p>
                 <br />
-                <p>
+                <p className='text-justify'>
                     I've explored HTML, CSS, JavaScript, PHP, MySQL, and frameworks like Bootstrap and Tailwind CSS. I've also ventured into back-end technologies like Node.js, Express.js, MongoDB, and MySQL. I'm not just a developer, I'm a problem solver. I enjoy the thrill of overcoming challenges and finding innovative solutions to complex issues.
                 </p>
             </div>
